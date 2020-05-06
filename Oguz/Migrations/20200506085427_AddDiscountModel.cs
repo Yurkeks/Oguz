7768e0610db1401a750eddf189419ade7eaa5978
Vladislav Korsak <vladkorsak11@mail.ru>
2020-05-06 11:55:10 +0300

    Add DiscountModel

---
+++ Oguz/Migrations/20200506085427_AddDiscountModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Oguz.Migrations
+{
+    public partial class AddDiscountModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Styles",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "SMTPClients",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Orders",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Customers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Colors",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Brands",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Discounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Active = table.Column<bool>(nullable: false),
+                    Value = table.Column<int>(nullable: false),
+                    LinearMeters = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discounts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Discounts");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Styles");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "SMTPClients");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Colors");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Brands");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
