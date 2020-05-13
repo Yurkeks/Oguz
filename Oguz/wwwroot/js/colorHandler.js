5dc46f1237242f83e824a1243be211c5aebfa13b
Vladislav Korsak <vladkorsak11@mail.ru>
2020-05-13 18:10:39 +0300

    set: set Default color in color view

--- Oguz/wwwroot/js/colorHandler.js
+++ Oguz/wwwroot/js/colorHandler.js
@@ -1,11 +1,31 @@
 ﻿document.addEventListener('DOMContentLoaded', colorHandler);
+document.addEventListener('DOMContentLoaded', setDefaultColor);
+
+
 function colorHandler() {
     var colors = document.querySelectorAll('.color');
     var colorId = document.getElementById('ColorId');
     colors.forEach((color) => {
         color.onclick = function () {
+            setActiveColor(colors, this);
             colorId.value = color.id;
         }
     });
 }
 
+function setActiveColor(colors, currentColor) {
+    colors.forEach((color) => {
+        color.classList.remove('active-color');
+    });
+    currentColor.classList.add('active-color');
+}
+
+function setDefaultColor() {
+    var colors = document.querySelectorAll('.color');
+    var colorId = document.getElementById('ColorId');
+    for (i = 0; i < colors.length; i++) {
+        colors[i].classList.add('active-color');
+        colorId.value = colors[i].id;
+        break;
+    }
+}
